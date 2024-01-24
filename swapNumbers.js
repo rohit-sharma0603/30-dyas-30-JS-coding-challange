16b6801864b61093e186b384b18c5a6ad8a6d4b8
rohit-sharma0603 <sharmarohit061997@gmail.com>
2024-01-25 00:03:42 +0530

    Create swapNumbers.js

---
+++ swapNumbers.js
@@ -0,0 +1,27 @@
+// // first method
+// function swapTwoNumbers(a,b){
+ 
+//     let temp=a;
+//     a=b;
+//     b=temp;
+//     return [a,b];
+// }
+
+// let x=10; let y=20;
+// [x,y]=swapTwoNumbers(x,y);
+
+// console.log(x,y);
+
+
+
+// // second method
+//       function swap(a,b){
+//         [a , b]=[b ,a];
+//         let temp=[a,b];
+//         return temp;
+//       }
+//       let x=10; let y=30;
+
+//       [x,y]=swap(x,y);
+
+//       console.log(x,y);
