a3b4c1898687627205af8b565002b0c24c7af104
rohit-sharma0603 <sharmarohit061997@gmail.com>
2024-02-11 20:29:01 +0530

    Create genrateOtp.js

---
+++ genrateOtp.js
@@ -0,0 +1,9 @@
+function generateOtp(length){
+    let OTP="";
+    for(let i=0;i<length; i++){
+        let randomNumber=Math.floor(Math.random()*10);
+        OTP=OTP+randomNumber.toString();
+    }
+    return OTP;
+}
+console.log(generateOtp(5));
