74a1e9b43366f4f876d5047d04ec2090692ce362
rohit-sharma0603 <sharmarohit061997@gmail.com>
2024-05-01 07:28:56 +0530

    Update allowOneFunctionCall.js

--- allowOneFunctionCall.js
+++ allowOneFunctionCall.js
@@ -44,3 +44,51 @@ var once = function(fn) {
  * onceFn(1,2,3); // 6
  * onceFn(2,3,6); // returns undefined without calling fn
  */
+
+
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+//     The first time the returned function is called, it should return the same result as fn.
+//     Every subsequent time it is called, it should return undefined.
+
+ 
+
+// Example 1:
+
+// Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
+
+// Example 2:
+
+// Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],
+
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called=false;
+    let result;
+    return function(...args){
+        if(!called){
+            result=fn(...args);
+            called=true;
+            return result
+        }else{
+            return undefined;
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
